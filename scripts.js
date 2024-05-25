982436121d55d62094c324a9e03822a443d6221d
Forcattt <72027797+Forcattt@users.noreply.github.com>
2024-05-25 23:21:26 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+// scripts.js
+
+// Function to display a greeting message
+function displayGreeting() {
+    alert("Hello! Welcome to our website.");
+}
+
+// Function to validate the contact form
+function validateForm() {
+    var nameInput = document.getElementById("name").value;
+    var classNumInput = document.getElementById("class_num").value;
+
+    // Perform validation
+    return true
+}
+
